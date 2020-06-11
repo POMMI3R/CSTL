815ade49da3049b65a421ceb53c9c6f639ded65a
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 05:36:14 +0900

    Create option_example.c

---
+++ examples/option_example.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <utility/option.h>
+
+#define CASE    break; case
+#define DEFAULT break; default
+
+OPTION_DEF(int);
+
+Option(int) try_division(int lhs, int rhs) {
+    // return rhs ? some(int, lhs / rhs) : none(int);
+    return rhs ? option(int, lhs / rhs) : option(int);
+}
+
+int main() {
+    int lhs = 0, rhs = 0;
+
+    scanf("%d %d", &lhs, &rhs);
+
+    Option(int) division = try_division(lhs, rhs);
+
+    switch (division.tag) {
+    CASE SOME: printf("Ok! %d\n", division.value);
+    CASE NONE: printf("Error!\n");
+    }
+
+    return 0;
+}
