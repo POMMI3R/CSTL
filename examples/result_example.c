d57805835ae03654aa0e5acd0d82e329b4554d9b
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 05:01:13 +0900

    Update result_example.c

--- examples/result_example.c
+++ examples/result_example.c
@@ -1,18 +1,29 @@
+
 #include <stdio.h>
 #include <utility/result.h>
 
 #define CASE    break; case
 #define DEFAULT break; default
 
-RESULT_DEF(int, char*);
+typedef enum {
+    DIVISION_BY_ZERO,
+    INVALID_OPERATOR
+} Error;
 
-RESULT(int, char*) eval(int lhs, int rhs, char op) {
+char* error_message[] = {
+    "Division by zero",
+    "Invalid operator"
+};
+
+RESULT_DEF(int, Error);
+
+RESULT(int, Error) eval(int lhs, int rhs, char op) {
     switch (op) {
-    CASE '+': return OK_NEW(int, char*, lhs + rhs);
-    CASE '-': return OK_NEW(int, char*, lhs - rhs);
-    CASE '*': return OK_NEW(int, char*, lhs * rhs);
-    CASE '/': return rhs ? OK_NEW(int, char*, lhs / rhs) : ERR_NEW(int, char*, "Division by zero");
-    DEFAULT : return ERR_NEW(int, char*, "Invalid operator");
+    CASE '+': return OK_NEW(int, Error, lhs + rhs);
+    CASE '-': return OK_NEW(int, Error, lhs - rhs);
+    CASE '*': return OK_NEW(int, Error, lhs * rhs);
+    CASE '/': return rhs ? OK_NEW(int, Error, lhs / rhs) : ERR_NEW(int, Error, DIVISION_BY_ZERO);
+    DEFAULT : return ERR_NEW(int, Error, INVALID_OPERATOR);
     }
 }
 
@@ -22,11 +33,11 @@ int main() {
 
     scanf("%d %c %d", &lhs, &op, &rhs);
 
-    RESULT(int, char*) result = eval(lhs, rhs, op);
+    RESULT(int, Error) result = eval(lhs, rhs, op);
 
     switch (result.tag) {
     CASE OK : printf("Ok! %d\n", result.ok);
-    CASE ERR: printf("Error! %s\n", result.err);
+    CASE ERR: printf("Error! %s\n", error_message[ result.err ]);
     }
 
     return 0;
