04bf267f856f39f2024f7e4d0dffcf83f83e34aa
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 05:35:21 +0900

    Update option.h

--- cstl/include/utility/option.h
+++ cstl/include/utility/option.h
@@ -7,51 +7,50 @@
 
 #define NUMARGS(...) (sizeof((int[]){ __VA_ARGS__ }) / sizeof(int))
 
-#define OPTION_DEF(T)                               \
-OPTION_IMPL(T)                                      \
-NONE_NEW_IMPL(T)                                    \
-SOME_NEW_IMPL(T)                                    \
-OPTION_NEW_IMPL(T)
+#define OPTION(T)                                       \
+__STRUCT_OPTION_IMPL(T)                                 \
+__FUNC_NONE_IMPL(T)                                     \
+__FUNC_SOME_IMPL(T)                                     \
+__FUNC_OPTION_IMPL(T)
 
-#define OPTION(T) OPTION_NAME(T)
-#define OPTION_NAME(T) Option_##T
-#define OPTION_IMPL(T)                              \
-typedef struct {                                    \
-    enum { NONE, SOME } tag;                        \
-    T value;                                        \
-} OPTION_NAME(T);
+#define Option(T) __STRUCT_OPTION_NAME(T)
+#define __STRUCT_OPTION_NAME(T) __Option##T
+#define __STRUCT_OPTION_IMPL(T)                         \
+typedef struct {                                        \
+    enum { NONE, SOME } tag;                            \
+    T value;                                            \
+} __STRUCT_OPTION_NAME(T);
 
-#define NONE_NEW(T)
-#define NONE_NEW_NAME(T) none_##T
-#define NONE_NEW_IMPL(T)                            \
-OPTION(T) NONE_NEW_NAME(T)(void) {                  \
-    OPTION(T) ret = {                               \
-        .tag   = NONE                               \
-        .value = 0;                                 \
-    }; return ret;                                  \
+#define none(T) __FUNC_NONE_NAME(T)()
+#define __FUNC_NONE_NAME(T) __none_##T
+#define __FUNC_NONE_IMPL(T)                             \
+Option(T) __FUNC_NONE_NAME(T)(void) {                   \
+    Option(T) ret = {                                   \
+        .tag   = NONE,                                  \
+        .value = 0                                      \
+    }; return ret;                                      \
 }
 
-#define SOME_NEW(T, V) SOME_NEW_NAME(T)(V)
-#define SOME_NEW_NAME(T) some_##T
-#define SOME_NEW_IMPL(T)                            \
-OPTION(T) SOME_NEW_NAME(T)(T value) {               \
-    OPTION(T) ret = {                               \
-        .tag   = SOME,                              \
-        .value = value                              \
-    }; return ret;                                  \
+#define some(T, V) __FUNC_SOME_NAME(T)(V)
+#define __FUNC_SOME_NAME(T) __some_##T
+#define __FUNC_SOME_IMPL(T)                             \
+Option(T) __FUNC_SOME_NAME(T)(T value) {                \
+    Option(T) ret = {                                   \
+        .tag   = SOME,                                  \
+        .value = value                                  \
+    }; return ret;                                      \
 }
 
-#define OPTION_NEW(T, ...) OPTION_NEW_NAME(T)(NUMARGS(__VA_ARGS__), ##__VA_ARGS__)
-#define OPTION_NEW_NAME(T) option_##T
-#define OPTION_NEW_IMPL(T)                          \
-OPTION(T) OPTION_NEW_NAME(T)(ssize_t args, ...) {   \
-    if (!args)                                      \
-        return NONE_NEW(T);                         \
-    va_list vl;                                     \
-    va_start(vl, args);                             \
-    OPTION(T) ret = SOME_NEW(T, va_arg(vl, T));     \
-    va_end(vl);                                     \
-    return ret;                                     \
+#define option(T, ...) __FUNC_OPTION_NAME(T)(NUMARGS(__VA_ARGS__), ##__VA_ARGS__)
+#define __FUNC_OPTION_NAME(T) __option_##T
+#define __FUNC_OPTION_IMPL(T)                           \
+Option(T) __FUNC_OPTION_NAME(T)(ssize_t args, ...) {    \
+    if (!args) return none(T);                          \
+    va_list vl;                                         \
+    va_start(vl, args);                                 \
+    Option(T) ret = some(T, va_arg(vl, T));             \
+    va_end(vl);                                         \
+    return ret;                                         \
 }
 
 #endif
