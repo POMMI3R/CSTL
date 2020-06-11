de5ff378715a2840b4fe4227f3f5dbd051b374bc
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 04:43:55 +0900

    Update and rename cstl/include/result.h to cstl/include/utility/result.h

--- cstl/include/utility/result.h
+++ cstl/include/utility/result.h
@@ -8,7 +8,7 @@ OK_NEW_IMPL(T, U)                                   \
 ERR_NEW_IMPL(T, U)
 
 #define RESULT(T, U) RESULT_NAME(T, U)
-#define RESULT_NAME(T, U) Result_##T
+#define RESULT_NAME(T, U) __Result##T
 #define RESULT_IMPL(T, U)                           \
 typedef struct {                                    \
     enum { OK, ERR } tag;                           \
@@ -19,19 +19,21 @@ typedef struct {                                    \
 } RESULT_NAME(T, U);
 
 #define OK_NEW(T, U, V) OK_NEW_NAME(T, U)(V)
-#define OK_NEW_NAME(T, U) ok_##T
+#define OK_NEW_NAME(T, U) __ok_##T
 #define OK_NEW_IMPL(T, U)                           \
 RESULT(T, U) OK_NEW_NAME(T, U)(T ok) {              \
     RESULT(T, U) ret = {                            \
-        .ok = ok                                    \
+        .tag = OK,                                  \
+        .ok  = ok                                   \
     }; return ret;                                  \
 }
 
 #define ERR_NEW(T, U, V) ERR_NEW_NAME(T, U)(V)
-#define ERR_NEW_NAME(T, U) err_##T
+#define ERR_NEW_NAME(T, U) __err_##T
 #define ERR_NEW_IMPL(T, U)                          \
 RESULT(T, U) ERR_NEW_NAME(T, U)(U err) {            \
     RESULT(T, U) ret = {                            \
+        .tag = ERR,                                 \
         .err = err                                  \
     }; return ret;                                  \
 }
