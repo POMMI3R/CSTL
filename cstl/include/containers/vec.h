dc0e30db715347e1d0adbb290fe1e15b5bb2dbb7
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 07:19:42 +0900

    Update vec.h

--- cstl/include/containers/vec.h
+++ cstl/include/containers/vec.h
@@ -7,29 +7,29 @@
 
 #define __NPT(V) (1 << 32 - __builtin_clz(V - 1))
 
-#define VEC(T)                                          \
-__STRUCT_VEC_IMPL(T)                                    \
-__FUNC_VEC1_IMPL(T)                                     \
-__FUNC_VEC2_IMPL(T)                                     \
+#define VEC(T)                                                  \
+__STRUCT_VEC_IMPL(T)                                            \
+__FUNC_VEC1_IMPL(T)                                             \
+__FUNC_VEC2_IMPL(T)                                             \
 __FUNC_VEC3_IMPL(T)
 
 #define Vec(T) __STRUCT_VEC_NAME(T)
 #define __STRUCT_VEC_NAME(T) __Vec##T
-#define __STRUCT_VEC_IMPL(T)                            \
-typedef struct {                                        \
-    ssize_t size, capacity;                             \
-    T* data;                                            \
+#define __STRUCT_VEC_IMPL(T)                                    \
+typedef struct {                                                \
+    ssize_t size, capacity;                                     \
+    T* data;                                                    \
 } __STRUCT_VEC_NAME(T);
 
 #define vec1(T) __FUNC_VEC1_NAME(T)()
 #define __FUNC_VEC1_NAME(T) __vec1_##T
-#define __FUNC_VEC1_IMPL(T)                             \
-Vec(T) __FUNC_VEC1_NAME(T)(void) {                      \
-    Vec(T) ret = {                                      \
-        .size     = 0,                                  \
-        .capacity = 1,                                  \
-        .data     = malloc(sizeof(T))                   \
-    }; return ret;                                      \
+#define __FUNC_VEC1_IMPL(T)                                     \
+Vec(T) __FUNC_VEC1_NAME(T)(void) {                              \
+    Vec(T) ret = {                                              \
+        .size     = 0,                                          \
+        .capacity = 1,                                          \
+        .data     = malloc(sizeof(T))                           \
+    }; return ret;                                              \
 }
 
 #define vec2(T, C) __FUNC_VEC2_NAME(T)(C)
