ad69ba7425b3432100771ee556a6623641302815
Pommier1117 <61049940+Pommier1117@users.noreply.github.com>
2020-06-12 05:43:23 +0900

    Update result.h

--- cstl/include/utility/result.h
+++ cstl/include/utility/result.h
@@ -2,40 +2,40 @@
 #ifndef RESULT_H
 #define RESULT_H
 
-#define RESULT_DEF(T, U)                            \
-RESULT_IMPL(T, U)                                   \
-OK_NEW_IMPL(T, U)                                   \
-ERR_NEW_IMPL(T, U)
+#define RESULT(T, U)                                    \
+__STRUCT_RESULT_IMPL(T, U)                              \
+__FUNC_OK_IMPL(T, U)                                    \
+__FUNC_ERR_IMPL(T, U)
 
-#define RESULT(T, U) RESULT_NAME(T, U)
-#define RESULT_NAME(T, U) __Result##T##U
-#define RESULT_IMPL(T, U)                           \
-typedef struct {                                    \
-    enum { OK, ERR } tag;                           \
-    union {                                         \
-        T ok;                                       \
-        U err;                                      \
-    };                                              \
-} RESULT_NAME(T, U);
+#define Result(T, U) __STRUCT_RESULT_NAME(T, U)
+#define __STRUCT_RESULT_NAME(T, U) __Result##T##U
+#define __STRUCT_RESULT_IMPL(T, U)                      \
+typedef struct {                                        \
+    enum { OK, ERR } tag;                               \
+    union {                                             \
+        T ok;                                           \
+        U err;                                          \
+    };                                                  \
+} __STRUCT_RESULT_NAME(T, U);
 
-#define OK_NEW(T, U, V) OK_NEW_NAME(T, U)(V)
-#define OK_NEW_NAME(T, U) __ok_##T##U
-#define OK_NEW_IMPL(T, U)                           \
-RESULT(T, U) OK_NEW_NAME(T, U)(T ok) {              \
-    RESULT(T, U) ret = {                            \
-        .tag = OK,                                  \
-        .ok  = ok                                   \
-    }; return ret;                                  \
+#define ok(T, U, V) __FUNC_OK_NAME(T, U)(V)
+#define __FUNC_OK_NAME(T, U) __ok_##T##U
+#define __FUNC_OK_IMPL(T, U)                            \
+Result(T, U) __FUNC_OK_NAME(T, U)(T ok) {               \
+    Result(T, U) ret = {                                \
+        .tag = OK,                                      \
+        .ok  = ok                                       \
+    }; return ret;                                      \
 }
 
-#define ERR_NEW(T, U, V) ERR_NEW_NAME(T, U)(V)
-#define ERR_NEW_NAME(T, U) __err_##T##U
-#define ERR_NEW_IMPL(T, U)                          \
-RESULT(T, U) ERR_NEW_NAME(T, U)(U err) {            \
-    RESULT(T, U) ret = {                            \
-        .tag = ERR,                                 \
-        .err = err                                  \
-    }; return ret;                                  \
+#define err(T, U, V) __FUNC_ERR_NAME(T, U)(V)
+#define __FUNC_ERR_NAME(T, U) __err_##T##U
+#define __FUNC_ERR_IMPL(T, U)                           \
+Result(T, U) __FUNC_ERR_NAME(T, U)(U err) {             \
+    Result(T, U) ret = {                                \
+        .tag = ERR,                                     \
+        .err = err                                      \
+    }; return ret;                                      \
 }
 
 #endif
